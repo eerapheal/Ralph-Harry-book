2f158ee710399e054e9c958a3222828f26cafe4e
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-09-26 23:10:07 +0300

    Modified css

--- book.js
+++ book.js
@@ -52,8 +52,7 @@ class bookLibrary {
     this.books.forEach((book, index) => {
       const h3 = document.createElement('h3');
       const authorNames = document.createElement('p');
-      const line = document.createElement('hr');
-    
+          
       const removeBtn = document.createElement('button');
       h3.textContent = `Book: ${book.title} `;
       authorNames.textContent = `Author: ${book.author}`;
